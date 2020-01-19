a568b03a68aa68a04b8983a7dfaefccce0e2ca18
Brian Begy <brian.begy@kazoohr.com>
2020-01-19 12:19:25 -0600

    cleanup and consolidate

--- src/aoc2019/day01.clj
+++ src/aoc2019/day01.clj
@@ -4,12 +4,29 @@
             [clojure.math.numeric-tower :as math]
             [aoc2019.helpers :as helpers]))
 
-(defn calculateModuleFuel
+(defn calculateModuleFuelSimple
   [module]
   (- (math/floor (/ module 3)) 2))
 
-(defn doit
+
+(defn calculateModuleFuelWithFuelCost
+  [module]
+  (let [f (calculateModuleFuelSimple module)]
+    (if (> f 0)
+      (if (>= f 3)
+        (+ f (calculateModuleFuelWithFuelCost f))
+        f)
+      0)))
+
+(defn doSimpleChallenge
   []
   (reduce
-   (fn [c, m] (+ c (calculateModuleFuel m)))
+   (fn [c, m] (+ c (calculateModuleFuelSimple m)))
+   0 (helpers/getArrayOfIntegersFromFile "data/day01.txt")))
+
+
+(defn doComplexChallenge
+  []
+  (reduce
+   (fn [c, m] (+ c (calculateModuleFuelWithFuelCost m)))
    0 (helpers/getArrayOfIntegersFromFile "data/day01.txt")))
--- test/aoc2019/day01/tests.clj
+++ test/aoc2019/day01/tests.clj
@@ -2,8 +2,27 @@
   (:require [clojure.test :refer :all]
             [aoc2019.day01]))
 
-(deftest day1 test
+(deftest test-calculateModuleFuelSimple
+  (are [input result] (= result (aoc2019.day01/calculateModuleFuelSimple input))
+    12 2
+    14 2
+    1969 654
+    100756 33583))
+
+(deftest test-doSimpleChallenge
   (is
    (=
-    (aoc2019.day01/doit)
+    (aoc2019.day01/doSimpleChallenge)
     3368364N)))
+
+
+(deftest test-calculateModuleFuelWithFuelCost
+  (are [input result] (= result (aoc2019.day01/calculateModuleFuelWithFuelCost input))
+    14 2
+    1969 966
+    100756 50346))
+
+
+(deftest completeChallenge
+  (are [result] (= result (aoc2019.day01/doComplexChallenge))
+    5049684))
